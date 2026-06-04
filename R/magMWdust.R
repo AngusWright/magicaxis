c0fbebc1a84c9c2d7b6b0987d4dcfc917e17b96c
Angus H. Wright <awright@astro.rub.de>
2026-06-04 09:57:06 +0200

    update magMWdust to use lazy-loaded SFD_dust lookup rather than direct read

--- R/magMWdust.R
+++ R/magMWdust.R
@@ -9,13 +9,24 @@
 
 
 # Define a plotting helper that overlays Milky Way dust on a projected sky plot.
-magMWdust <- function(type = "p", pch = 16, pt.cex = 0.5, opacity.range = c(0, 0.5), whiteblack.percentile = c(0.5, 0.95), stretch = "lin", min.opacity.plot = 0.01, show.status = TRUE, ...) {
+magMWdust <- function(dust.data = NULL, type = "p", pch = 16, pt.cex = 0.5, opacity.range = c(0, 0.5), whiteblack.percentile = c(0.5, 0.95), stretch = "lin", min.opacity.plot = 0.01, show.status = TRUE, ...) {
 
   # Restrict the drawing style to points or polygons.
   if (!type %in% c("pl", "p")) stop("magMWdust function expects type of 'p' (for points) or 'pl' (for polygons) only")
-  # Read the dust map data, which is a dlon=dlat=1 sampling  
-  dlon <- dlat <- 1
-  dust_all<-read.csv(system.file("extdata","SFD_dust.csv", package = "magicaxis"))
+  # If none provided, read the dust map data, which is a dlon=dlat=1 sampling  
+  if (is.null(dust.data)) { 
+    dlon <- dlat <- 1
+    # Lazy load the SFD_dust data
+    dust_all<-SFD_dust
+  } else { 
+    if (!is.data.frame(dust.data)) {
+      stop("dust.data is not a data frame; load an example with data(SFD_dust)") 
+    }
+    if (!all(c("ebv","ra","dec")%in%colnames(dust.data))) {
+      stop("dust.data is missing required components; load an example with data(SFD_dust)") 
+    }
+    dust_all<-dust.data 
+  }
   # Map dust values onto an opacity scale for plotting.
   dust_all$map <- magicaxis::magmap(dust_all$ebv, range = opacity.range, hicut = whiteblack.percentile[2], locut = whiteblack.percentile[1], stretch = stretch)$map
   # Drop grid cells that would be too faint to plot usefully.
