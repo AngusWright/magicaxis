987c0b049cf4043f5a861be67aa2a04cb5759e72
Angus H. Wright <awright@astro.rub.de>
2026-06-04 09:14:52 +0200

    remove unnecessary eq2gal function

--- R/magMWdust.R
+++ R/magMWdust.R
@@ -42,39 +42,3 @@ magMWdust <- function(type = "p", pch = 16, pt.cex = 0.5, opacity.range = c(0, 0
   return(invisible(NULL))
 }
 
-#Convert equatorial coordinates to galactic coordinates 
-.eq2gal<-function(alpha,delta,equinox='J2000') { 
-  if (equinox=='J2000') { 
-    alpha0<-192.8595
-    delta0<-27.1284
-    ell0<-122.9320
-  } else if (equinox=='B1950') { 
-    alpha0<-192.25
-    delta0<-27.40
-    ell0<-123.000
-  } else { 
-    stop("unknown equinox provided: must be either J2000 or B1950") 
-  }
-
-  dcos<-function(theta) cos(theta*pi/180)
-  dsin<-function(theta) sin(theta*pi/180)
-  datan2<-function(f,x) atan2(f,x)*180/pi
-  dasin<-function(f) asin(f)*180/pi
-
-  alpha_prime=alpha-alpha0
-  delta_prime=delta-delta0
-
-  b = (dasin(dsin(delta)*dsin(delta0)+dcos(delta)*dcos(delta0)*dcos(alpha_prime)))
-
-  calcy = (dcos(delta)*dsin(alpha_prime))
-  calcx = (dsin(delta)*dcos(delta0)-dcos(delta)*dsin(delta0)*dcos(alpha_prime))
-
-  ell = -datan2(calcy,calcx)+ell0
-
-  ell[which(ell<0)]<-ell[which(ell<0)]+360
-  ell[which(ell>360)]<-ell[which(ell>360)]-360
-
-  return=cbind(ell,b)
-
-}
-
